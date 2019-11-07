3f94e907943d31b31e4d0a51cafa32f4a4507def
Sujish Pradeep <sujishp164@gmail.com>
2019-11-07 14:35:46 +1100

    Initial set up

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-2",
+    BUCKET: "notes-app-uploads-785"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-2",
+    URL: "https://8tybbwcg0h.execute-api.ap-southeast-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "ap-southeast-2",
+    USER_POOL_ID: "ap-southeast-2_XUQDibQyP",
+    APP_CLIENT_ID: "4s3a1i5l7ijnjvam0ciuoi3l6r",
+    IDENTITY_POOL_ID: "ap-southeast-2:bf858e92-fee7-410e-b54c-c47d780965e7"
+  }
+};
